6dc8242c07e4f8958f146a0cc7e8256c34471549
Gayatri27 <gpise@mail.sfsu.edu>
2018-09-26 17:15:44 -0700

    Database setup

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV === 'development') {
+require("dotenv").config();
+}
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -6,7 +10,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var tests = require('./routes/tests');
 var app = express();
 
 // view engine setup
@@ -22,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/tests', tests);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
