ba9b3ef357f8993a59be79ca051a92014777cf10
Dylan Wright <sealsurlaw@gmail.com>
2018-10-16 16:37:04 -0700

    Database test with ejs

--- app.js
+++ app.js
@@ -11,12 +11,13 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var tests = require('./routes/tests');
+var vertical = require('./routes/vertical');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+//app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/tests', tests);
+app.use('/vertical', vertical);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/vertical.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var router = express.Router();
+var pgp = require('pg-promise');
+var db = require('../db');
+
+
+
+
+router.get('/', function(req, res, next) {
+  //res.send('respond with a resource');
+  db.any('SELECT * FROM item')
+    .then(function(myData) {
+      console.log(myData);
+      res.render('vertical', { data: myData });
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+});
+module.exports = router;
