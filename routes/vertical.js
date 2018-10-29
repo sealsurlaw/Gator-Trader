9d0e9d51a0efefe2e9bd318e3db1e90a330d2b95
Dylan Wright <sealsurlaw@gmail.com>
2018-10-29 16:19:48 -0700

    Commented vertical prototype

--- routes/vertical.js
+++ routes/vertical.js
@@ -5,57 +5,76 @@ var router = express.Router();
 var db = require('../db');
 
 router.get('/', function(req, res, next) {
+  // Get all categories from database
   db.any(`SELECT * FROM category`)
+    // Query returns with data called 'cat'
     .then(function(cat) {
       // Get query from url
       var q = url.parse(req.url, true).query;
+      // Store search query if exists
       var search = q.search;
+      // Store browse query if exists
       var browse = q.browse;
-      var queryData, numReturned;
+      // Number of results returned from database
+      var numReturned;
 
       // Display page with results if search query or browse
 
       // Search by name
       if (search) {
+        // Get items from database where item title and item description have
+        // some part of it matching the search query
         db.any(`SELECT * FROM item WHERE item_title ILIKE '%` +
           search + `%' OR item_description ILIKE '%` + search + `%'`)
+          // Query returns with data called 'myData'
           .then(function(myData) {
             // How many search results returned
             numReturned = myData.length;
+            // Go to page 'vertical.ejs' passing in selected items, number of search results, and categories
             res.render('vertical', { data: myData, size: numReturned, categories: cat });
           })
           .catch(function(error) {
-              console.log(error);
+
+            console.log(error);
           });
       }
 
-      // search by category
+      // Browse by category
       else if (browse) {
+        // Default value for all categories is '-1'
         if (browse == -1) {
+          // Get all items in the database
           db.any(`SELECT * FROM item`)
+            // Query returns with data called 'myData'
             .then(function(myData) {
             // How many browse results returned
             numReturned = myData.length;
+            // Go to page 'vertical.ejs' passing in selected items, number of search results, and categories
             res.render('vertical', { data: myData, size: numReturned, categories: cat });
           })
           .catch(function(error) {
-              console.log(error);
+            // Print out error
+            console.log(error);
           });
         }
         else {
+          // Get items in database that have category ID defined in url
           db.any(`SELECT * FROM item WHERE category_id=` + browse)
+            // Query returns with data called 'myData'
             .then(function(myData) {
               // How many browse results returned
               numReturned = myData.length;
               res.render('vertical', { data: myData, size: numReturned, categories: cat });
             })
             .catch(function(error) {
-                console.log(error);
+              // Print out error
+              console.log(error);
             });
           }
-      }
+        }
 
       else {
+        // Go to page 'vertical.ejs' passing no search / no browse defaults
         res.render('vertical', { data: undefined, size: -1, categories: cat });
       }
   });
