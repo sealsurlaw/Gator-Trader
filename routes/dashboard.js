891d44ecbc9ded270f2083001dc72dd0883e1287
Dylan Wright <sealsurlaw@gmail.com>
2018-11-29 11:41:23 -0800

    Connected messages to dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -4,24 +4,58 @@ var db = require('../db');
 var render = require("../models/loginCheck").renderUserAndCategory;
 
 router.get('/', function(req, res, next) {
-  if (req.session.user_id) {
-    db.any(`SELECT * FROM item WHERE user_id =` + req.session.user_id )
-    .then( data => db.any(`SELECT * from category`)
-    .then( cats => {
-      data.forEach(item => {
-        cats.forEach(cat => {
-          if (item.category_id == cat.category_id) {
-            item.item_category = cat.category_name;
-          }
-        });
-      });
-      render(req, res, 'dashboard', 'DASHBOARD PAGE','dashboard', {data: data});
-    }));
-  }
-  else {
+  if (!req.session.user_id) {
     req.session.nextPage = '/dashboard';
     res.redirect('/login');
+    return;
   }
+
+  db.any(`SELECT * FROM item WHERE user_id =` + req.session.user_id )
+  .then( data => db.any(`SELECT * from category`)
+  .then( cats => {
+
+    // Items
+    data.forEach(item => {
+      cats.forEach(cat => {
+        if (item.category_id == cat.category_id) {
+          item.item_category = cat.category_name;
+        }
+      });
+    });
+
+    // Messages
+    if (data.length > 0) {
+      var where = ' WHERE ';
+      data.forEach(element => {
+        where += 'item_id='+element.item_id+' OR ';
+      });
+      where = where.substr(0, where.length-4);
+      db.any(`SELECT * FROM gator_message`+where)
+      .then( messages => {
+        // Get usernames associated with IDs
+        where = ' WHERE ';
+        messages.forEach(element => {
+          where += 'user_id='+element.user_id+' OR ';
+        });
+        where = where.substr(0, where.length-4);
+        db.any(`SELECT user_id, user_name FROM user_record`+where)
+        .then( usernames => {
+          // Map username with message
+          messages.forEach( element => {
+            usernames.forEach( element2 => {
+              if (element.user_id == element2.user_id)
+                element.user_name = element2.user_name;
+            });
+          });
+          data.messages = messages;
+          render(req, res, 'dashboard', 'DASHBOARD PAGE','dashboard', {data: data});
+        })
+      })
+    }
+    else {
+      render(req, res, 'dashboard', 'DASHBOARD PAGE','dashboard', {data: data});
+    }
+  }));
 
 });
 
