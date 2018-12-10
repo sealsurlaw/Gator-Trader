2d82f6b392e6adec099cb04d703c9815f275e82d
Charul Ramtekkar <r.charul114@gmail.com>
2018-12-10 15:20:24 -0800

    Added comments to item.js and login.js

--- routes/item.js
+++ routes/item.js
@@ -1,3 +1,8 @@
+/*
+* The purpose of this .js file to redirect to a specific item that
+* was clicked on by the user. Depeding on the item_id, we get
+* the item and its details from the database.
+*/
 var express = require("express");
 var router = express.Router();
 var db = require('../db');
@@ -5,6 +10,7 @@ var render = require("../models/loginCheck").renderUserAndCategory;
 
 router.get('/:id', function(req, res, next) {
     var id = req.params.id;
+    //Get item and category depending on the item clicked from the URL
     db.any(`SELECT * FROM category`)
     .then( cat => db.any(`SELECT * FROM item WHERE item_id=` + id)
     .then( data => {
@@ -12,6 +18,7 @@ router.get('/:id', function(req, res, next) {
             if (element.category_id == data[0].category_id)
                 data[0].category = element.category_name;
         });
+        //Go to a new page called details page to show the item 
         render(req, res, 'details', 'DETAILS PAGE', 'details', {data: data[0]});
         // res.render( 'details', {data: data[0], title: 'DETAILS PAGE', stylesheet: 'details', categories: cat, category: category} );
     }))
--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,12 @@
+/*
+* The purpose of this .js file is to redirect users to the login page
+* where they are presented with a form to fill in their credentials.
+*/
+
 var express = require("express");
 var router = express.Router();
 var db = require('../db');
 var formidable = require('formidable');
-
 var loginUser = require('../models/loginCheck').loginUser;
 
 //uses post method
@@ -10,6 +14,7 @@ router.post('/', function(req, res, next) {
     console.log(req.signedCookies);
     var form = new formidable.IncomingForm();
 
+    //Parse the information from the form and get the user_record from the database
     form.parse(req,function(err,fields,files){
         db.any(`SELECT * FROM user_record WHERE user_email='` + fields.email + `'`)
         .then(function (user){
