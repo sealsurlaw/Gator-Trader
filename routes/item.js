8fe6672647bf3e7851af1be0becb5d5ff3994c7f
Dylan Wright <sealsurlaw@gmail.com>
2018-12-10 19:26:49 -0800

    Control access to details page. Made / and /home the same. Alphabetical categories.

--- routes/item.js
+++ routes/item.js
@@ -5,18 +5,26 @@ var render = require("../models/loginCheck").renderUserAndCategory;
 
 router.get('/:id', function(req, res, next) {
     var id = req.params.id;
+    var user_id = req.session.user_id;
+    if (!user_id) user_id = -1;
     db.any(`SELECT * FROM category`)
     .then( cat => db.any(`SELECT * FROM item WHERE item_id=` + id)
-    .then( data => {
-        cat.forEach(element => {
-            if (element.category_id == data[0].category_id)
-                data[0].category = element.category_name;
-        });
-        render(req, res, 'details', 'DETAILS PAGE', 'details', {script: 'details', data: data[0]});
-        // res.render( 'details', {data: data[0], title: 'DETAILS PAGE', stylesheet: 'details', categories: cat, category: category} );
-    }))
+    .then( data => db.any(`SELECT * FROM user_record WHERE user_id=`+user_id)
+    .then( user => {
+        if (data[0].item_status == 'Approved' || user[0].admin_right == true || user[0].user_id == data[0].user_id) {
+            cat.forEach(element => {
+                if (element.category_id == data[0].category_id)
+                    data[0].category = element.category_name;
+            });
+            render(req, res, 'details', 'DETAILS PAGE', 'details', {script: 'details', data: data[0]});
+        }
+        else {
+            res.redirect('/');
+        }
+    })))
     .catch( (e) => {
-        res.render('error');
+        console.log(e);
+        res.redirect('/');
     });
 });
 
