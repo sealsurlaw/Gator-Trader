06b2611995fc3289b0b87e532e51a31a7dcdc3ad
Charul Ramtekkar <r.charul114@gmail.com>
2018-12-16 13:32:01 -0800

    added comments to register.js

--- public/javascript/register.js
+++ public/javascript/register.js
@@ -2,7 +2,6 @@
 * This is where we check if the password is valid and and where
 * we remove any spaces from the entered username.
 */
-
 function passwordValidation(){
     var Pswd  = document.getElementById("UserPswd");
     var rPswd = document.getElementById("RePswd");
@@ -12,7 +11,9 @@ function passwordValidation(){
     }
     return true;
 }
-// Removes spaces from username
+/*
+* This function removes spaces from username
+*/
 function removeSpaces(string) {
     return string.split(' ').join('');
 }
