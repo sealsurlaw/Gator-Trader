7bf4f692312241be513ff850c88aec281f6a881f
Charul Ramtekkar <r.charul114@gmail.com>
2018-12-16 13:49:26 -0800

    Added comments to the tabSwitcher.js and search-header.js

--- public/javascript/search-header.js
+++ public/javascript/search-header.js
@@ -1,4 +1,10 @@
-window.onload = function() {	
+/*
+* The purpose of this .js file is for the search header which is
+* consistent in all tabs. Depending on the category chosen the
+* user can get the items in the same.
+*/
+
+window.onload = function() {
 	var urlParams = new URLSearchParams(window.location.search);
 
     if (urlParams.has('category')) {
